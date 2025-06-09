f13a002eb5c7cae509408ab1210744fe3ce87491
Matheus <43159599+MatheusGoms@users.noreply.github.com>
2025-06-09 15:36:55 -0300

    Update celular.cs

--- celular.cs
+++ celular.cs
@@ -7,3 +7,23 @@ public abstract class Celular
     public abstract void FazerChamada(string numero);
     public abstract void EnviarMensagem(string numero, string mensagem);
 }
+
+public class ModeloA1 : Celular
+{
+    public ModeloA1()
+    {
+        Marca = "Marca A";
+        Modelo = "Modelo A1";
+        SistemaOperacional = "Android";
+    }
+
+    public override void FazerChamada(string numero)
+    {
+        Console.WriteLine($"Chamada feita para {numero} usando {Modelo}.");
+    }
+
+    public override void EnviarMensagem(string numero, string mensagem)
+    {
+        Console.WriteLine($"Mensagem enviada para {numero}: {mensagem}");
+    }
+}
